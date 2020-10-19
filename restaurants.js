352c747ffbb3a0fc5d59016c60ca99c1b7b60a1e
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-10-19 19:47:48 -0400

    Created alternate page idea and alternate js and style

--- restaurants.js
+++ restaurants.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
 	$(".owl-carousel").owlCarousel();
   });
 
+// This is the geolocator for restaurants nearest you //
+  var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
   // Create a request variable and assign a new fetch object to it.
 var request = new XMLHttpRequest()
 
