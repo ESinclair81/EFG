4357f44526f2748ca23e0c5f86339ee2dab1a5e1
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-10-19 19:02:47 -0400

    updated some js

--- restaurants.js
+++ restaurants.js
@@ -3,3 +3,17 @@
 $(document).ready(function(){
 	$(".owl-carousel").owlCarousel();
   });
+
+  // Create a request variable and assign a new fetch object to it.
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function () {
+  // Begin accessing JSON data here
+
+}
+
+// Send request
+request.send()
