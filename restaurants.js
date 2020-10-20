ef7a7a089eec2cdeff6677fd45b68c6e2a264c84
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-10-20 00:51:11 -0400

    api key updates

--- restaurants.js
+++ restaurants.js
@@ -20,6 +20,18 @@ function showPosition(position) {
   "<br>Longitude: " + position.coords.longitude;
 }
 
+
+//Display Result on a map //
+function showPosition(position) {
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+  "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
+
+  document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
+
   // Create a request variable and assign a new fetch object to it.
 var request = new XMLHttpRequest()
 
