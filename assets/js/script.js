d37c9af8a6c3caa4faa9a506a34481ceef1e5dcb
Ghardlotte <g_hardlotte@hotmail.com>
2020-10-24 18:32:05 -0600

    added new api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-
-
-
+var searchFormEl = $("#searchform")
+var searchInputEl = $("#movie-search");
+//var apiKey = hrt8j7mezsyzdh6ezyn3bn2t
 
 var settings = {
     "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=10",
@@ -12,7 +12,7 @@ var settings = {
     "Authorization": "Basic U0NIT18yOF9YWDo3eUZMS3pTdklQcmg=",
     "client": "SCHO_28 ",
     "x-api-key": "iEgMtXm1Gc8WHC1cP0KMC5Qi7hTY1xuY21MRlNM7",
-    "device-datetime": "2020-10-21T09:28:57.296Z",
+    "device-datetime": "2020-10-23T09:50:57.296Z",
     "territory": "XX",
     "geolocation": "-22.0;14.0", 
     },
@@ -26,11 +26,6 @@ var settings = {
             content.append('<li>' + film['film_name'] + '</li>');
         }
 
-
-
-        
-    
-     
     console.log(response);
    
     });
@@ -39,7 +34,7 @@ var settings = {
 
 
     var nearBy = {
-        "url": "https://api-gate2.movieglu.com/cinemasNearby/?n5",
+        "url": "https://api-gate2.movieglu.com/cinemasNearby/?n=5",
         "method": "GET",
         "timeout": 0,
         "datatype": "JSON",
@@ -48,20 +43,64 @@ var settings = {
         "Authorization": "Basic U0NIT18yOF9YWDo3eUZMS3pTdklQcmg=",
         "client": "SCHO_28 ",
         "x-api-key": "iEgMtXm1Gc8WHC1cP0KMC5Qi7hTY1xuY21MRlNM7",
-        "device-datetime": "2020-10-21T09:28:57.296Z",
+        "device-datetime": "2020-10-23T10:20:57.296Z",
         "territory": "XX",
         "geolocation": "-22.0;14.0", 
         },
        };
 
+
        $.ajax(nearBy).done(function (response) {
 
         var cinemas = response.cinemas;
-        var cinemasNear = $("#cinemasNear");
+        var cinemasNear = $("#cinemas");
 
         for (var i = 0; i < cinemas.length; ++i) {
-            var cinemas = cinemas[i];
-            cinemasNear.append('<li>' + cinemas['film_name'] + '</li>');
-        }
+           var cinema = cinemas[i];
+            cinemasNear.append('<li>' + cinema['cinema_name'] + '</li>');
+       }
            console.log(response);
        });
+
+
+       var showTimes = {
+        "url": "https://api-gate2.movieglu.com/filmShowTimes/?film_id=7772&date=2020-10-24&n=10",
+        "method": "GET",
+        "timeout": 0,
+        "datatype": "JSON",
+        "headers": {
+        "api-version": "v200",
+        "Authorization": "Basic U0NIT18yOF9YWDo3eUZMS3pTdklQcmg=",
+        "client": "SCHO_28 ",
+        "x-api-key": "iEgMtXm1Gc8WHC1cP0KMC5Qi7hTY1xuY21MRlNM7",
+        "device-datetime": "2020-10-23T10:20:57.296Z",
+        "territory": "XX",
+        "geolocation": "-22.0;14.0", 
+        },
+       };
+
+       $(document).ready(function() {
+           $("searchform").submit(function(){
+           });
+           $("button").click(function(){
+               $("movie-search").submit(showTimes)
+           })
+       })
+
+       $.ajax(showTimes).done(function(response){
+
+        var filmShowTimes = response.film;
+        var filmShowTime = $("#showtimes");
+
+        for(i = 0; i < cinemas.length; ++i) {
+            var filmTimes = filmShowTimes[i]
+            filmShowTime.append("<li>" + filmTimes["showings"] + "</li>");
+        }
+        console.log(response);
+        
+       });
+
+       
+
+
+      // searchInputEl.addEventListener("submit", formSubmitHandler);
